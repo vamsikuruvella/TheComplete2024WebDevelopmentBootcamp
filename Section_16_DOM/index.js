96603d1d9c0fd845c91a3de9bf104584762024f3
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-08-30 09:45:54 +0000

    cc

--- Section_16_DOM/index.js
+++ Section_16_DOM/index.js
@@ -1,3 +1,8 @@
 window.onload=function(){
     alert("Hello From index.js")
+
+    var tagname=document.getElementsByTagName("li");
+    var tagnamelen=tagname.length;
+    var classname=document.getElementsByClassName('btn')
+    console.log("Tag Name: "+tagname+"\nTag Name Len: "+tagnamelen+"\nClass Name: "+classname);
 }
