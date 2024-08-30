22a37fec6fd572470dd003b484c145494a56df56
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-08-30 10:57:42 +0000

    qq

--- Section_16_DOM/index.js
+++ Section_16_DOM/index.js
@@ -9,9 +9,9 @@ window.onload=function(){
     var queryname=document.querySelector("#title");//"#list a","li a","a",
     var queryallname=document.querySelectorAll("#list .item");
     console.log("Tag Name: "+tagname+"\nTag Name Len: "+tagnamelen+"\nClass Name: "+classname+"\nClass Name Len: "+classnamelen+"\nID Name: "+idname+"\nQuery Name: "+queryname+"\nQuery all Name: "+queryallname);
-    var claslistname=idname.classList
-    console.log("Class List: "+claslistname);
-    classname.classList.toggle('vvvv');
+    // var claslistname=idname.classList
+    // console.log("Class List: "+claslistname);
+    // classname.classList.toggle('vvvv');
     console.log(queryname.innerHTML);
     console.log(queryname.textContent);
     queryallname[0].setAttribute('href',"www.github.com");
