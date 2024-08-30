eb2e0bb920771e7e2f751c72dc0651d42d5c1f84
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-08-30 09:58:24 +0000

    bb

--- Section_16_DOM/index.js
+++ Section_16_DOM/index.js
@@ -5,6 +5,7 @@ window.onload=function(){
     var tagnamelen=tagname.length;
     var classname=document.getElementsByClassName('btn');
     var classnamelen=classname.length;
-    var idname=document.getElementById()
-    console.log("Tag Name: "+tagname+"\nTag Name Len: "+tagnamelen+"\nClass Name: "+classname+"\nClass Name Len: "+classnamelen);
+    var idname=document.getElementById("list");
+    var queryname=document.querySelector("#title");
+    console.log("Tag Name: "+tagname+"\nTag Name Len: "+tagnamelen+"\nClass Name: "+classname+"\nClass Name Len: "+classnamelen+"\nID Name: "+idname+"\nQuery Name: "+queryname);
 }
