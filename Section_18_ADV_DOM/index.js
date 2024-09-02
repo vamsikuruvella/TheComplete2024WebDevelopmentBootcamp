6e2e851813b15dde58aaaebb938a10c603ba7be9
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-02 10:41:06 +0000

    vv

--- Section_18_ADV_DOM/index.js
+++ Section_18_ADV_DOM/index.js
@@ -6,6 +6,6 @@
 var buttons=document.querySelectorAll("button")
 for(i in buttons){
     buttons[i].addEventListener("click",function(){
-        alert("I got clicked.")
+        alert("I got clicked.");
     })
 }
