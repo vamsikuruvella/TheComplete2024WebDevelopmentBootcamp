9d18f6a4a7ecbe8199d4a8e3877c0546bb7c1228
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-02 10:48:30 +0000

    1

--- Section_18_ADV_DOM/index.js
+++ Section_18_ADV_DOM/index.js
@@ -1,11 +1,18 @@
 // window.onload(){}
 
-
-
-
 var buttons=document.querySelectorAll("button")
 for(i in buttons){
     buttons[i].addEventListener("click",function(){
         alert("I got clicked.");
     })
+}
+//Higher order functions
+function add(n1,n2){
+    return n1+n2;
+}
+function multiply(n1,n2){
+    return n1*n2;
+}
+function Calc(n1,n2,oper){
+    oper(n1,n2);
 }
