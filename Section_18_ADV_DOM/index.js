d0cd27ba922682add7a13905567abad1371a35cd
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-10 14:21:18 +0000

    vv

--- Section_18_ADV_DOM/index.js
+++ Section_18_ADV_DOM/index.js
@@ -13,8 +13,42 @@ window.onload = function() {
     console.log(bellboy2.name);
     console.log(bellboy2.experience);
 
-    document.addEventListener("keypress",function(){
-        alert("Key Pressed");
+    document.addEventListener("keypress",function(event){
+        console.log(event);
+        var buttonInnerHTMl = event.key;
+
+        switch (buttonInnerHTMl) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var tom5 = new Audio("sounds/snare.mp3");
+                tom5.play();
+                break;
+            case "k":
+                var tom6 = new Audio("sounds/crash.mp3");
+                tom6.play();
+                break;
+            case "l":
+                var tom7 = new Audio("sounds/kick-bass.mp3");
+                tom7.play();
+                break;
+            default:
+                console.log(buttonInnerHTMl);
+        }
     });
 }
 
