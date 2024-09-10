7a3753cea2558ab1ff6115118d812c27d4ed25db
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-10 09:58:40 +0000

    vv

--- Section_18_ADV_DOM/index.js
+++ Section_18_ADV_DOM/index.js
@@ -14,12 +14,37 @@ for(i in buttons){
         // var audio=new Audio("sounds/tom-1.mp3");
         // audio.play();
         var buttonInnerHTMl=this.innerHTML
-        switch buttonInnerHTMl:
+        switch (buttonInnerHTMl){
         case "w":
-            var audio=new Audio("sounds/tom-1.mp3");
-            audio.play();
-        break:
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+         break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            var tom5=new Audio("sounds/tom-5.mp3");
+            tom5.play();
+        break;
+        case "k":
+            var tom6=new Audio("sounds/tom-6.mp3");
+            tom6.play();
+        break;
+        case "l":
+            var tom7=new Audio("sounds/tom-7.mp3");
+            tom7.play();
+        break;
         default:
+        }
     })
 }
 //Higher order functions
