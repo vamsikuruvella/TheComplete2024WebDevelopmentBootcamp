03ba3656d363d07248af721bdfc04dacc51b0dd8
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-15 07:41:53 +0000

    nn

--- Section_19_JQuery/index.js
+++ Section_19_JQuery/index.js
@@ -7,6 +7,6 @@
 // })
 
 console.log($('h1').css('color'));
-$('h1').css('color','green');
+// $('h1').css('color','green');
 $('h1').addClass('big-title margin-50'); //.removeClass()
 $('h1').attr('id','big-tt');
