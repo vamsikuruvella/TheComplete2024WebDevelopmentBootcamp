73360d44cbb5ebbfbccda416a17e6c40163c344a
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-20 09:27:44 +0000

    tt

--- Section_23_NodeJS/index.js
+++ Section_23_NodeJS/index.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+const fs=require("fs");
+
+fs.writeFile("message.txt","Hello From NodeJS",function(err){
+    if(err){
+        throw err;
+    }
+    console.log("File saved!")
+})
