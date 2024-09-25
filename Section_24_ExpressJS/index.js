85bb141fb1e968c069ab747c3761b750799fb0e8
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-25 10:11:32 +0000

    ll

--- Section_24_ExpressJS/index.js
+++ Section_24_ExpressJS/index.js
@@ -3,7 +3,17 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-    res.send("Hello, World!");
+    res.send("<h1>Hello, World!</h1>");
+    console.log(req.rawHeaders);
+});
+
+app.get('/about',(req,res)=>{
+    res.send("<h1>About Me!</h1>");
+    console.log(req.rawHeaders);
+});
+
+app.get('/contact',(req,res)=>{
+    res.send("<h1>Contact me!</h1>");
     console.log(req.rawHeaders);
 });
 
