540301b4b5e552ba4b4fb9770024edb61ed20a7d
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-09-30 10:46:27 +0000

    ee

--- Section_24_ExpressJS/secrets_Reveal/index.js
+++ Section_24_ExpressJS/secrets_Reveal/index.js
@@ -1,3 +1,9 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express"
+import morgan from "morgan"
+
+const app=express();
+const port=3000
+
