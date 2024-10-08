ec2d8bda0f542064024e79575517d93d1304851a
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-08 10:46:07 +0000

    rr

--- Section_25_EJS/index.js
+++ Section_25_EJS/index.js
@@ -4,7 +4,7 @@ const app=express()
 const port=3000
 
 app.get('/',(req,res)=>{
-    res.render("index.ejs",{dayType:"a weekday",advice:"It's time to work hard."});
+    res.render("index.ejs",{dayType:"a weekday",advice:"It's time to work hard.",});
 
 })
 
