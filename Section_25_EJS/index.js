91b52a072ed66456d7421d7cd644608b8622e432
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-08 10:43:22 +0000

    ii

--- Section_25_EJS/index.js
+++ Section_25_EJS/index.js
@@ -3,9 +3,9 @@ import express from "express"
 const app=express()
 const port=3000
 
-app.length('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render("index.ejs",{dayType:"a weekday",advice:"It's time to work hard."});
-    
+
 })
 
 app.listen(port,()=>{
