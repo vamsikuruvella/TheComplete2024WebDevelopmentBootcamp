eccc1b669ba4f72d382b218d4609ba9aa64864e0
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-09 10:40:39 +0000

    rr

--- Section_25_EJS/index2.js
+++ Section_25_EJS/index2.js
@@ -3,8 +3,10 @@ import express from "express"
 const app=express()
 const port=3000
 
+let bowl=['oranges','apples','grapes']
+
 app.get('/',(req,res)=>{
-    
+    res.render('index2.ejs',{ fruits:bowl, })
 })
 
 app.listen(port,()=>{
