aa41597db1e129bfa13c10eddbb9a8c241fba7bd
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-09 10:36:51 +0000

    qq

--- Section_25_EJS/index2.js
+++ Section_25_EJS/index2.js
@@ -3,7 +3,9 @@ import express from "express"
 const app=express()
 const port=3000
 
-
+app.get('/',(req,res)=>{
+    
+})
 
 app.listen(port,()=>{
     console.log(`Server Running on Port ${port}`)
