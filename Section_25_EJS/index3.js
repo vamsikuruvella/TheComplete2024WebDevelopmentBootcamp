f946444fe30311f3af8db9fc2adef6a7ec8d91a1
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-10 10:41:39 +0000

    ii

--- Section_25_EJS/index3.js
+++ Section_25_EJS/index3.js
@@ -1,8 +1,20 @@
 import express from "express"
+import bodyParser from "body-parser"
 
 const app=express()
 const port=3000
 
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.render('index3.ejs')
+});
+
+app.post('/submit',(req,res)=>{
+    const number_of_letters= (req.body["fName"]+req.body["lName"]).length;
+    console.log(number_of_letters)
+    res.render('index3.ejs',{numberofletters:number_of_letters})
+});
 
 
 app.listen(port,()=>{
