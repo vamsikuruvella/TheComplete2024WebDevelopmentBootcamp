3548bce8526707f09b918b5c4c79328df98ea831
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-10-25 11:46:50 +0000

    oo

--- Section_25_EJS/index5.js
+++ Section_25_EJS/index5.js
@@ -4,6 +4,10 @@ import bodyParser from "body-parser"
 const app=express()
 const port=3000
 
+app.get('/',(req,res)=>{
+    // res.locals.number_of_letters=1;
+    res.render('index5.ejs')
+});
 
 
 
